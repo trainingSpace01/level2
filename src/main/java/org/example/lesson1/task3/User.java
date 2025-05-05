e1ca3e26d6a361211a45f5732b66c2cbd7d36655
SokolDZR <33631961+SokolDZR@users.noreply.github.com>
2025-05-05 21:19:16 +0300

    добавлена 1 урок для 2 курса

--- src/main/java/org/example/lesson1/task3/User.java
+++ src/main/java/org/example/lesson1/task3/User.java
@@ -1,16 +1,36 @@
 package org.example.lesson1.task3;
 
+import com.github.javafaker.Faker;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.UUID;
 
 @Getter
-@AllArgsConstructor
+@ToString
 public class User {
-    private String uniqueID;
+    @ToString.Exclude
+    private final Faker faker = new Faker(Locale.FRANCE);
+    private int uniqueID;
     private String name;
     private int age;
     private ArrayList<Phone> phones;
+
+    public User (int id) {
+        this.name = faker.name().firstName();
+        //this.name = "John"; // для первого таска
+        this.age = faker.number().numberBetween(15, 18);
+        this.uniqueID = id;
+        this.phones = new ArrayList<>();
+        for (int i = 0; i < randomSize(); i++){
+            phones.add(new Phone());
+        }
+    }
+
+    private int randomSize(){
+        return (int)(Math.random() * 3) + 1;
+    }
 }
---
+++ src/main/java/org/example/lesson1/task4/SearchService.java
@@ -0,0 +1,4 @@
+package org.example.lesson1.task4;
+
+public interface SearchService {
+}
